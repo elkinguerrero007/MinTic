2d090f5dc29d79553d99fe24aec857af4f387e13
elkinguerrero007 <3439@holbertonschool.com>
2022-08-20 07:58:46 -0500

    reto3

--- ciclo_2/Reto_3/Reto3.java
+++ ciclo_2/Reto_3/Reto3.java
@@ -1,41 +1,34 @@
-import java.util.Scanner;
 import java.util.HashMap;
+import java.util.Scanner;
 
+class Reto3 {
 
-class Reto3{
+    private final Scanner scanner = new Scanner(System.in); 
 
-
-    private final Scanner scanner = new Scanner(System.in);
-
-
-    public String read(){
+    public String read() {
         return this.scanner.nextLine();
     }
 
-    
-    public void run(){
-        
+    public void run() {
+
         BaseDatosProductos ListaProductos = new BaseDatosProductos();
-        String operacion=read();
-        String [] entrada = read().split(" ");
-        Productos productos = new Productos(Integer.parseInt(entrada [0]), entrada [1], Double.parseDouble(entrada[2]),
-            Integer.parseInt(entrada[3]));
-            boolean salida = false;
-         
-            if(operacion.equals("AGREGAR")) {
-           salida = ListaProductos.agregar(productos.getCodigo(), productos);
-        }        
-            else if(operacion.equals("ACTUALIZAR")){
+        String operacion = read();
+        String[] entrada = read().split(" ");
+        Productos productos = new Productos(Integer.parseInt(entrada[0]), entrada[1], Double.parseDouble(entrada[2]),
+                Integer.parseInt(entrada[3]));
+        boolean salida = false;
+
+        if (operacion.equals("AGREGAR")) {
+            salida = ListaProductos.agregar(productos.getCodigo(), productos);
+        } else if (operacion.equals("ACTUALIZAR")) {
             salida = ListaProductos.actualizar(productos.getCodigo(), productos);
-        }
-            else if(operacion.equals("BORRAR")){
+        } else if (operacion.equals("BORRAR")) {
             salida = ListaProductos.eliminar(productos.getCodigo(), productos);
         }
-            if(salida) {
-                System.out.println(String.format("%.1f", ListaProductos.valor_inventario()));
-                
-        }
-            else{            
+        if (salida) {
+            ListaProductos.generarInforme();
+
+        } else {
             System.out.println("ERROR");
         }
 
@@ -43,12 +36,13 @@ class Reto3{
 }
 
 class BaseDatosProductos {
-    private HashMap<Integer,Productos> ListaProductos = new HashMap<Integer, Productos>();
-    
-    BaseDatosProductos(){
-        
+
+    private HashMap<Integer, Productos> ListaProductos = new HashMap<Integer, Productos>();
+
+    BaseDatosProductos() {
+
         this.ListaProductos.put(1, new Productos(1, "Manzanas", 5000.0, 25));
-        this.ListaProductos.put(2, new Productos(2, "Limones",  2300.0, 15));
+        this.ListaProductos.put(2, new Productos(2, "Limones", 2300.0, 15));
         this.ListaProductos.put(3, new Productos(3, "Peras", 2700.0, 33));
         this.ListaProductos.put(4, new Productos(4, "Arandanos", 9300.0, 5));
         this.ListaProductos.put(5, new Productos(5, "Tomates", 2100.0, 42));
@@ -58,43 +52,69 @@ class BaseDatosProductos {
         this.ListaProductos.put(9, new Productos(9, "Chocolates", 3500.0, 80));
         this.ListaProductos.put(10, new Productos(10, "Jamon", 15000.0, 10));
     }
-    
-    
-    public boolean agregar(int codigo, Productos productos){
+
+    public boolean agregar(int codigo, Productos productos) {
         if (this.ListaProductos.containsKey(codigo)) {
             return false;
         }
         this.ListaProductos.put(codigo, productos);
         return true;
-        }       
-    
-     
-    public boolean actualizar(int codigo, Productos productos){
+    }
+
+    public boolean actualizar(int codigo, Productos productos) {
         if (this.ListaProductos.containsKey(codigo)) {
             this.ListaProductos.replace(codigo, productos);
             return true;
-        }       
+        }
         return false;
     }
 
-   public boolean eliminar(int codigo, Productos productos){
+    public boolean eliminar(int codigo, Productos productos) {
         if (this.ListaProductos.containsKey(codigo)) {
             this.ListaProductos.remove(codigo);
             return true;
-        }       
+        }
         return false;
     }
-   public double valor_inventario(){
+
+    public double valor_inventario() {
         double contador = 0;
-        for(Integer i: ListaProductos.keySet()) {
-           contador+=(ListaProductos.get(i).getPrecio()*ListaProductos.get(i).getInventario());
-    }
+        for (Integer i : ListaProductos.keySet()) {
+            contador += (ListaProductos.get(i).getPrecio() * ListaProductos.get(i).getInventario());
+        }
         return contador;
-   } 
+    }
+
+    public void generarInforme() {
+        double sumatoriaValores = 0;
+        Double precioMayor = 0.0;
+        Double precioMenor = 9999999.9;
+        String nombreMayor = "";
+        String nombreMenor = "";
+
+        for (Productos producto : ListaProductos.values()) {
+            if (producto.getPrecio() > precioMayor) {
+                precioMayor = producto.getPrecio();
+                nombreMayor = producto.getNombre();
+            }
+
+            if (producto.getPrecio() < precioMenor) {
+                precioMenor = producto.getPrecio();
+                nombreMenor = producto.getNombre();
+            }
+
+            sumatoriaValores += producto.getPrecio();
+        }
+
+        Double promedio = sumatoriaValores / ListaProductos.size();
+
+        System.out.println(nombreMayor + " " + nombreMenor + " " + String.format("%.1f", promedio));
+
+    }
 }
 
-
 class Productos {
+
     int Codigo;
     String Nombre;
     double Precio;
@@ -138,69 +158,4 @@ class Productos {
     public void setInventario(int Inventario) {
         this.Inventario = Inventario;
     }
-
-     public String generarInforme_3() {
-        int llave = (new ArrayList<Integer>(listaProducto.keySet())).get(0);
-        Producto productoMayor = listaProducto.get(llave);
-        Producto productoMenor = productoMayor;
-        double sumatoria = 0;
-        int contador = 0;
-        for (Producto x : listaProducto.values()) {
-            // Para El mayor
-            if (x.getPrecio() > productoMayor.getPrecio()) {
-                productoMayor = x;
-            }
-            // Para el menor
-            if (x.getPrecio() < productoMenor.getPrecio()) {
-                productoMenor = x;
-            }
-            // Para el promedio
-            sumatoria = sumatoria + x.getPrecio();
-            contador++;
-        }
-        double promedio = sumatoria / contador;
-        String resultado = productoMayor.getNombre() + " " + productoMenor.getNombre() + " " + String.format("%.1f", promedio);
-        return resultado;
-    }
-
-    public String generarInforme_2() {
-        String mayor = this.productoMayor();
-        String menor = this.productoMenor();
-        double promedio = this.promedio();
-
-        return mayor + " " + menor + " " + String.format("%.1f", promedio);
-    }
-
-    public String agregar(int codigo, String nombre, double precio, int inventario) {
-        if (listaProducto.containsKey(codigo)) {
-            return "ERROR";
-        } else {
-            Producto x = new Producto(codigo, nombre, precio, inventario);
-            listaProducto.put(codigo, x);
-            return generarInforme_2();
-        }
-    }
-
-    public String borrar(int codigo, String nombre, double precio, int inventario) {
-        if (listaProducto.containsKey(codigo)) {
-            listaProducto.remove(codigo);
-            return generarInforme_2();
-        } else {
-            return "ERROR";
-        }
-    }
-
-    public String actualizar(int codigo, String nombre, double precio, int inventario) {
-        if (listaProducto.containsKey(codigo)) {
-            Producto x = listaProducto.get(codigo);
-            x.setNombre(nombre);
-            x.setPrecio(precio);
-            x.setInventario(inventario);
-            listaProducto.put(codigo, x);
-            return generarInforme_2();
-        } else {
-            return "ERROR";
-        }
-    }
-
 }
